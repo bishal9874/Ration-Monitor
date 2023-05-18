4f0db4a4e3013a032e413a3f7940c3b6ac53f0e4
Bishal Mondal <bm8130377@gmail.com>
2023-05-18 10:33:41 +0530

    userkyc

--- project-UI-with-React/src/components/AuthScreens/userKYC/UserKyc.jsx
+++ project-UI-with-React/src/components/AuthScreens/userKYC/UserKyc.jsx
@@ -11,6 +11,8 @@ import {
   Box,
   Alert,
   Typography,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import "./userKyc.css";
 import { useState, useEffect } from "react";
@@ -19,6 +21,11 @@ import { getToken, storeToken } from "../../../API/localStorage";
 import { useUser_kycMutation } from "../../../API/rationApi";
 import { useDispatch } from "react-redux";
 import { setUsertoken } from "../../../features/authSlice";
+import {
+  states,
+  districtsByState,
+  village_town,
+} from "../../../utils/CountryData";
 const UserKyc = () => {
   const [server_error, setServerError] = useState({});
   const navigate = useNavigate();
@@ -30,12 +37,17 @@ const UserKyc = () => {
 
     const data = new FormData(e.currentTarget);
     const kycData = {
+      // dateofbirth:data.get("date_of_birth"),
+      // gender:data.get("gender"),
+      // state:data.get("state"),
+      // district:data.get("district"),
       village: data.get("village"),
       houseNo: data.get("houseNo"),
       post_office: data.get("post_office"),
       pin: data.get("pin"),
       Annual_income: data.get("Annual_income"),
     };
+    console.log(kycData);
     const res = await kycuser(kycData);
     console.log(res);
     if (res.error) {
@@ -50,6 +62,23 @@ const UserKyc = () => {
       navigate("/dashboard");
     }
   };
+  const [state, setState] = useState("");
+  const [district, setDistrict] = useState("");
+  const [villageTown, setVillage_town] = useState("");
+
+  const handleStateChange = (event) => {
+    setState(event.target.value);
+    setDistrict("");
+  };
+
+  const handleDistrictChange = (event) => {
+    setDistrict(event.target.value);
+  };
+  const handleVillageTown = (event) => {
+    setVillage_town(event.target.value);
+  };
+
+  //Aadhar No varifiy
 
   return (
     <>
@@ -69,8 +98,8 @@ const UserKyc = () => {
       </section>
       <section className="paddings flexCenter innerWidth uk-container">
         <img src="src/assets/kyc.png" width={600} height={400} />
+        
         <div className="userkyc_form">
-          
           <Box
             component="form"
             noValidate
@@ -80,8 +109,42 @@ const UserKyc = () => {
             id="userkyc-form"
             onSubmit={handlekycuser}
           >
+          
+              <section className="inputDividers">
+                <div>
+                                {/* Aadhar card Number */}
             <TextField
-              margin="normal"
+              fullWidth
+              id="aadharNumber"
+              name="aadharNumber"
+              label="Aadhar Number"
+              inputProps={{
+                maxLength: 14,
+                pattern: "\\d{4} \\d{4} \\d{4}",
+                title: "Please enter Aadhar number in format XXXX XXXX XXXX",
+              }}
+              onChange={(e) => {
+                const value = e.target.value;
+                const newValue = value.replace(/[^0-9]/g, ""); // remove non-numeric characters
+                let formattedValue = "";
+                for (let i = 0; i < newValue.length; i++) {
+                  if (i % 4 === 0 && i > 0) formattedValue += " "; // add space after every 4 characters
+                  formattedValue += newValue[i];
+                }
+                e.target.value = formattedValue;
+              }}
+            />
+              {/* Phone number */}
+            <TextField
+              label="Phone Number"
+              variant="outlined"
+              fullWidth
+              id="phNumber"
+              name="phNumber"
+              inputProps={{ maxLength: 10 }}
+            />
+              {/* date of Birth */}
+            <TextField
               fullWidth
               id="date_of_birth"
               name="date_of_birth"
@@ -91,6 +154,7 @@ const UserKyc = () => {
                 shrink: true,
               }}
             />
+            {/* gender */}
             <FormControl component="fieldset" margin="normal">
               <FormLabel component="legend">Gender</FormLabel>
               <RadioGroup row aria-label="gender" name="gender">
@@ -111,13 +175,76 @@ const UserKyc = () => {
                 />
               </RadioGroup>
             </FormControl>
-            <TextField
+              {/* state */}
+            <Select
+              value={state}
+              onChange={handleStateChange}
+              displayEmpty
+              fullWidth
+              id="state"
+              name="state"
+              label="State"
+            >
+              <MenuItem value="" disabled>
+                Select State
+              </MenuItem>
+              {states.map((state) => (
+                <MenuItem key={state} value={state}>
+                  {state}
+                </MenuItem>
+              ))}
+            </Select>
+                {/* district */}
+            {state && (
+              <Select
+                value={district}
+                onChange={handleDistrictChange}
+                displayEmpty
+                fullWidth
+                id="district"
+                name="district"
+                label="District"
+              >
+                <MenuItem value="" disabled>
+                  Select District
+                </MenuItem>
+                {districtsByState[state].map((district) => (
+                  <MenuItem key={district} value={district}>
+                    {district}
+                  </MenuItem>
+                ))}
+              </Select>
+            )}
+                </div>
+                <div>
+                  {/* village */}
+            {district && (
+              <Select
+                value={villageTown}
+                onChange={handleVillageTown}
+                displayEmpty
+                fullWidth
+                id="village"
+                name="village"
+                label="village/Town"
+              >
+                <MenuItem value="" disabled>
+                  Select Village/Town
+                </MenuItem>
+                {village_town[district].map((villageTown) => (
+                  <MenuItem key={villageTown} value={villageTown}>
+                    {villageTown}
+                  </MenuItem>
+                ))}
+              </Select>
+            )}
+            {/* <TextField
               margin="normal"
               fullWidth
               id="village"
               name="village"
               label="village/Town"
-            />
+            /> */}
             {server_error.village ? (
               <Typography
                 style={{ fontSize: 12, color: "red", textAlign: "center" }}
@@ -196,12 +323,21 @@ const UserKyc = () => {
             ) : (
               " "
             )}
+            <TextField
+              margin="normal"
+              fullWidth
+              id="fpsCode"
+              name="fpsCode"
+              label="FPS (Fair Price Shop) Code "
+            />
+                </div>
+              </section>
+            
             {/* <TextField margin='normal' disabled fullWidth id='pin' name='pin' label='pin'/> */}
-            <Box component="form">
-              <button type="submit" className="button ">
-                Join
-              </button>
-            </Box>
+
+            <button type="submit" className="button ">
+              Join
+            </button>
 
             {server_error[0] ? (
               <Alert severity="error">{server_error[0]}</Alert>
